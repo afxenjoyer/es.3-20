57cdf6ea4db2e8247c9e86c858ab1c843e39b561
Andrea Di Mingo <dimingo.andrea25@gmail.com>
2024-10-02 17:22:39 +0200

    Scritto il programma per l'esercizio 3.20

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int anno;
+
+    printf("Inserisci l'anno da verificare\n");
+    scanf("%d",&anno);
+
+    if(anno%4 == 0)
+    {
+        printf("L'anno da verificare è bisestile\n");
+    }
+    else
+    {
+        printf("L'anno da verificare non è bisestile\n");
+    }
+
+    return 0;
+}
